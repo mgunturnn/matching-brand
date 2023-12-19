2a28c3bd695f5a5d4baf5b3e06e76aca7e021e78
mgunturnn <guntur.nugraha@upi.edu>
2023-12-19 11:33:22 +0700

    ongoing create a cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,14 +1,118 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  TextInput,
+  Image,
+} from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
 
 const CartScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
+  const total = cart
+    ?.map((item) => item.price * item.quantity)
+    .reduce((curr, prev) => curr + prev, 0);
+  console.log(total);
   return (
-    <View>
-      <Text>CartScreen</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ marginTop: 55, flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          backgroundColor: "#D3A29D",
+          padding: 20,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginHorizontal: 7,
+            padding: 5,
+            gap: 10,
+            backgroundColor: "#E7C7C3",
+            borderRadius: 20,
+            height: 38,
+            flex: 1,
+          }}
+        >
+          <AntDesign
+            style={{ paddingLeft: 10 }}
+            name="search1"
+            size={22}
+            color="#FFFFFF"
+          />
+          <TextInput style={{ color: "#FFFFFF" }} placeholder="Search" />
+        </Pressable>
+      </View>
 
-export default CartScreen
+      <View style={{ padding: 10, flexDirection: "row", alignItems: "center" }}>
+        <Text style={{ fontSize: 18, fontWeight: "400" }}>Subtotal : </Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{total}</Text>
+      </View>
+      <Text style={{ marginHorizontal: 10 }}>EMI details available</Text>
 
-const styles = StyleSheet.create({})
+      <Pressable
+        style={{
+          backgroundColor: "#FFC72C",
+          padding: 10,
+          borderRadius: 5,
+          justifyContent: "center",
+          alignItems: "center",
+          marginHorizontal: 10,
+          marginTop: 10,
+        }}
+      >
+        <Text>Proceed to Buy ({cart.length}) items</Text>
+      </Pressable>
+
+      <Text
+        style={{
+          height: 1,
+          borderColor: "#D0D0D0",
+          borderWidth: 1,
+          marginTop: 16,
+        }}
+      />
+
+      <View style={{ marginHorizontal: 10 }}>
+        {cart?.map((item, index) => (
+          <View
+            style={{
+              backgroundColor: "white",
+              marginVertical: 10,
+              borderBottomColor: "#F0F0F0",
+              borderWidth: 2,
+              borderLeftWidth: 0,
+              borderTopWidth: 0,
+              borderRightWidth: 0,
+            }}
+            key={index}
+          >
+            <Pressable style={{marginVertical:10, flexDirection:"row", alignItems:"center"}}>
+              <View>
+                <Image
+                  style={{ width: 140, height: 140, resizeMode: "contain" }}
+                  source={{ uri: item?.image }}
+                />
+              </View>
+
+              <View>
+                <Text style={{width:150}}>{item?.title}</Text>
+              </View>
+            </Pressable>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default CartScreen;
+
+const styles = StyleSheet.create({});
